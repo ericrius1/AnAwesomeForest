7436de8054440e8638954bde4cfe998ff35263a0
Eric <Ericrius1@gmail.com>
2014-05-05 10:57:09 -0700

    yar

--- js/classes/e.Game.js
+++ js/classes/e.Game.js
@@ -6,7 +6,27 @@ e.Game = new Class({
     this.renderer = new THREE.WebGLRenderer({
       antialias: true
     });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
+    this.camera = new THREE.PerspectiveCamera(45, 1, 1, 10000);
+
+    this.scene = new THREE.Scene();
+    document.body.appendChild(this.renderer.domElement);
+
+    window.addEventListener('resize', this.onWindowResize, false);
+
+  },
+
+  animate: function() {
+    this.renderer.render(this.scene, this.camera);
+  },
+
+  onWindowResize: function() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
   }
+
+
 
 });
