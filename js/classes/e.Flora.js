9cf70a4a53e567a4ab6f581aa3f6a27973bb9915
Eric <Ericrius1@gmail.com>
2014-05-05 18:18:49 -0700

    tree

--- js/classes/e.Flora.js
+++ js/classes/e.Flora.js
@@ -8,5 +8,12 @@ e.Flora = new Class({
     var geo = new THREE.TubeGeometry(extrudePath, 10);
     var mesh = new THREE.Mesh(geo);
     this.game.scene.add(mesh);
+
+    extrudePath = new THREE.SplineCurve3([
+      new THREE.Vector3(0, 10, 0), new THREE.Vector3(-10, 10, 0)
+    ]);
+    geo = new THREE.TubeGeometry(extrudePath)
+    mesh = new THREE.Mesh(geo);
+    this.game.scene.add(mesh)
   }
 });
