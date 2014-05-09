df543b09498d37b7885255c58da5ecd81eeecbc6
Eric <Ericrius1@gmail.com>
2014-05-09 07:47:43 -0700

    leaves

--- js/classes/e.World.js
+++ js/classes/e.World.js
@@ -2,17 +2,8 @@ e.World = new Class({
 
   construct: function(options) {
     this.game = options.game;
-    var maxAnisotropy = this.game.renderer.getMaxAnisotropy();
-    var texture = THREE.ImageUtils.loadTexture("assets/grass.jpg");
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(512, 512);
-    texture.anistropy = maxAnisotropy;
 
-    var groundBasic = new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      map: texture
-    });
-    var ground = new THREE.Mesh(new THREE.PlaneGeometry(50000, 50000), groundBasic);
+    var ground = new THREE.Mesh(new THREE.PlaneGeometry(50000, 50000));
     ground.rotation.x = -Math.PI / 2;
     this.game.scene.add(ground);
 
